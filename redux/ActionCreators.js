e5032102a9056c258cdcf2ad7e1a90ecbed8472e
Jennifer Bowen <jrog814@gmail.com>
2020-11-28 13:06:05 -0500

    react native workshop 2

--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -152,3 +152,23 @@ export const addFavorite = campsiteId => ({
     type: ActionTypes.ADD_FAVORITE,
     payload: campsiteId
 });
+
+export const postComment = (campsiteId, rating, author, text) => dispatch => {
+    const date = new Date();
+    const newComment = {
+        campsiteId,
+        rating,
+        author,
+        text,
+        date: date.toISOString()
+    };
+
+    setTimeout(() => {
+        dispatch(addComment(newComment));
+    }, 2000);
+};
+
+export const addComment = newComment => ({
+    type: ActionTypes.ADD_COMMENT,
+    payload: newComment
+});
--- redux/comments.js
+++ redux/comments.js
@@ -1,13 +1,16 @@
+import { bindActionCreators } from 'redux';
 import * as ActionTypes from './ActionTypes';
 
 export const comments = (state = { errMess: null, comments: []}, action) => {
     switch (action.type) {
         case ActionTypes.ADD_COMMENTS:
             return {...state, errMess: null, comments: action.payload};
-
         case ActionTypes.COMMENTS_FAILED:
             return {...state, errMess: action.payload};
-
+            case ActionTypes.ADD_COMMENT:
+                const comment = action.payload;
+                comment.id = state.comments.length;
+                return {...state, comments: state.comments.concat(comment)};  
         default:
             return state;
     }
