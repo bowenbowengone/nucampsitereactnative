38bcc65fd476dac43d03795eae13ff11939dc1b5
Jennifer Bowen <jrog814@gmail.com>
2020-11-30 17:50:31 -0500

    Swipe and Delete Favorites

--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -172,3 +172,8 @@ export const addComment = newComment => ({
     type: ActionTypes.ADD_COMMENT,
     payload: newComment
 });
+
+export const deleteFavorite = campsiteId => ({
+    type: ActionTypes.DELETE_FAVORITE,
+    payload: campsiteId
+}); 
